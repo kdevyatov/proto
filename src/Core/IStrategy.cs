357a44f8e5646234d4cb783fb8b1c8df29259fc7
solomon9182 <solomon9182@gmail.com>
2022-10-28 13:56:03 +0300

    Test

--- src/Core/IStrategy.cs
+++ src/Core/IStrategy.cs
@@ -5,6 +5,11 @@ namespace CRM.Core
 {
     public interface IStrategy<T, N>
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
         Func<T, N> Resolve(T entity);
     }
 }
